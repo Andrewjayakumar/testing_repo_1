f4a1469b4b49b6cf3e2e95df579237cf0cc55260
Fortnite_Crypto <56381729+Andrewjayakumar@users.noreply.github.com>
2025-07-01 19:14:29 +0530

    Upgrade polyfills.ts to Angular 9

---
+++ angular_upgrade_output/2025_07_01_13_44_24/src/polyfills.ts
@@ -0,0 +1,11 @@
+import 'zone.js/dist/zone';  // Included with Angular CLI.
+
+/**
+ * BROWSER POLYFILLS
+ */
+import 'core-js/es/reflect';
+import 'core-js/proposals/reflect-metadata';
+
+/**
+ * APPLICATION IMPORTS
+ */
