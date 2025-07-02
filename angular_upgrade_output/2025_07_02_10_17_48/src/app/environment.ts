53a435233ed34f7ffc6e2f140ecfe62fb8968f4f
Fortnite_Crypto <56381729+Andrewjayakumar@users.noreply.github.com>
2025-07-02 15:48:36 +0530

    Upgrade environment.ts to Angular 9

---
+++ angular_upgrade_output/2025_07_02_10_17_48/src/app/environment.ts
@@ -0,0 +1,6 @@
+// Angular 9 environment configuration
+export const environment = {
+  production: false,
+  authApiUrl: 'https://metaluat.ascendion.com:8601/',
+  baseUrl: 'https://metaluat.ascendion.com:8600/'
+};
