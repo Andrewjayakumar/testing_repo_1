b8875097f2f3cb286783ae484db90411d732df91
Fortnite_Crypto <56381729+Andrewjayakumar@users.noreply.github.com>
2025-07-04 15:26:25 +0530

    Delete angular_upgrade_output directory

--- angular_upgrade_output/2025_07_02_10_17_48/src/app/environment.ts
+++
@@ -1,6 +0,0 @@
-// Angular 9 environment configuration
-export const environment = {
-  production: false,
-  authApiUrl: 'https://metaluat.ascendion.com:8601/',
-  baseUrl: 'https://metaluat.ascendion.com:8600/'
-};
