7acf4c60fe81abbba826331dbe8a598424485adf
Fortnite_Crypto <56381729+Andrewjayakumar@users.noreply.github.com>
2025-07-08 19:43:51 +0530

    Upgrade app.component.ts to Angular 9 syntax

--- angular_upgrade_output/2025_07_08_14_07_17/src/app/app.component.ts
+++ angular_upgrade_output/2025_07_08_14_07_17/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Angular 9 Upgrade';
 
-  ngOnInit() {
+  ngOnInit(): void {
     // Initialization logic here
   }
 }
