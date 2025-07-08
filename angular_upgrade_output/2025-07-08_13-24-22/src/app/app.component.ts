cfc9078c03e800867262f71312b0ea94327d7ac3
Fortnite_Crypto <56381729+Andrewjayakumar@users.noreply.github.com>
2025-07-08 18:54:52 +0530

    Angular 9 upgrade: src/app/app.component.ts

---
+++ angular_upgrade_output/2025-07-08_13-24-22/src/app/app.component.ts
@@ -0,0 +1,11 @@
+/* Upgraded Angular 9 version */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'UpgradedApp';
+}
