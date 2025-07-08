de21c70b09d66eed94ca9b13b47b30d67a13c085
Fortnite_Crypto <56381729+Andrewjayakumar@users.noreply.github.com>
2025-07-08 19:45:01 +0530

    Angular 9 upgrade - app.component.ts

--- angular_upgrade_output/2025_07_08_14_07_17/src/app/app.component.ts
+++ angular_upgrade_output/2025_07_08_14_07_17/src/app/app.component.ts
@@ -3,10 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'Angular 9 Upgrade';
+  title = 'My Angular 9 App';
 
   ngOnInit(): void {
     // Initialization logic here
