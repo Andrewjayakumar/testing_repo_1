b1560ae547c2ca67272a7c6725f63a3a4212e89d
Fortnite_Crypto <56381729+Andrewjayakumar@users.noreply.github.com>
2025-07-08 14:16:15 +0530

    Upgraded home.component.ts to Angular 9 standards

--- angular_upgrade_output/2025_07_08_08_45_20/src/app/home/home.component.ts
+++ angular_upgrade_output/2025_07_08_08_45_20/src/app/home/home.component.ts
@@ -2,12 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
   constructor() { }
 
   ngOnInit(): void {
-    // Initialization logic here
+    // Initialization logic for the home component
   }
+
 }
