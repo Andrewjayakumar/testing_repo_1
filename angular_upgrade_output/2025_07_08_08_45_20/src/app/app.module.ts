f293fc63ecf9e1669f89ad37fe995b78fa4c12c6
Fortnite_Crypto <56381729+Andrewjayakumar@users.noreply.github.com>
2025-07-08 14:16:32 +0530

    Upgrade Angular 3 app.module.ts to Angular 9 syntax

--- angular_upgrade_output/2025_07_08_08_45_20/src/app/app.module.ts
+++ angular_upgrade_output/2025_07_08_08_45_20/src/app/app.module.ts
@@ -1,16 +1,19 @@
+// Angular 9 upgraded app.module.ts
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
+    AppComponent,
     HomeComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
